c1225f0f1eeab162c38e1ffb97c9a05306057a54
Maximilian Beck <beck@ml.jku.at>
2023-11-02 16:11:29 +0100

    try using at::BFloat16 for torch tensor creation

--- src/playg_v2/main.cpp
+++ src/playg_v2/main.cpp
@@ -1,18 +1,23 @@
-# include <torch/torch.h>
-# include <iostream>
-# include "interface.h"
+#include <ATen/ATen.h>
+#include <iostream>
+#include <torch/extension.h>
+
+#include "interface.h"
 
 #define CUDA_DEVICE 5
 
 int main() {
-    printf("Creating tensor:\n");
-    auto options = torch::TensorOptions().dtype(torch::kFloat16).device(torch::kCUDA, CUDA_DEVICE);
-    torch::Tensor tensor = torch::rand({2, 3}, options);
-    std::cout << tensor << std::endl;
+  printf("Creating tensor:\n");
+  auto options = torch::TensorOptions()
+                     .dtype(torch::kFloat16)
+                     //  .dtype(at::BFloat16)
+                     .device(torch::kCUDA, CUDA_DEVICE);
+  torch::Tensor tensor = torch::rand({2, 3}, options);
+  std::cout << tensor << std::endl;
 
-    printf("Calling copykernel:\n");
-    torch::Tensor tensor2 = vlstm::interface::copykernel(tensor);
-    std::cout << tensor2 << std::endl;
+  printf("Calling copykernel:\n");
+  torch::Tensor tensor2 = vlstm::interface::copykernel(tensor);
+  std::cout << tensor2 << std::endl;
 
-    return 0;
+  return 0;
 }
