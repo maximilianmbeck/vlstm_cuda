7fbcf1e2d3f78efc93b6272cf9f407ab61ecc859
Maximilian Beck <beck@ml.jku.at>
2023-11-02 15:46:33 +0100

    compile cuda kernels

--- src/playg_v2/main.cpp
+++ src/playg_v2/main.cpp
@@ -1,8 +1,18 @@
 # include <torch/torch.h>
 # include <iostream>
+# include "interface.h"
+
+#define CUDA_DEVICE 5
 
 int main() {
-    torch::Tensor tensor = torch::rand({2, 3});
+    printf("Creating tensor:\n");
+    auto options = torch::TensorOptions().dtype(torch::kFloat16).device(torch::kCUDA, CUDA_DEVICE);
+    torch::Tensor tensor = torch::rand({2, 3}, options);
     std::cout << tensor << std::endl;
+
+    printf("Calling copykernel:\n");
+    torch::Tensor tensor2 = vlstm::interface::copykernel(tensor);
+    std::cout << tensor2 << std::endl;
+
     return 0;
 }
