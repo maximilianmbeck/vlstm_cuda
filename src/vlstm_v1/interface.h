adc61bd372561597f53a96ef714180992dc67e3e
Maximilian Beck <beck@ml.jku.at>
2023-11-03 16:00:11 +0100

    Add vlstm_v1. Debugging matrixMul with arbitrary shapes.

---
+++ src/vlstm_v1/interface.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// #include <torch/extension.h>
+
+namespace vlstm {
+
+using torch::Tensor;
+
+namespace interface {
+
+Tensor testkernel(const Tensor& mat_A);
+
+Tensor copykernel(const Tensor& mat_A);
+
+Tensor mmkernelv1(Tensor mat_A, Tensor mat_B);
+
+} // namespace interface
+
+} // namespace vlstm
---
+++ src/vlstm_v1/interface.py
@@ -0,0 +1,39 @@
+import os
+from pathlib import Path
+
+import torch
+
+from ..cuda_init import load
+
+filedir = Path(os.path.dirname(os.path.abspath(__file__)))
+
+
+class CppModule(object):
+    module = None
+
+    @classmethod
+    def instance(cls):
+        if cls.module is None:
+            cls.module = load(name="vlstm_v1", sources=[str(filedir / "interface.cc"), str(filedir / "kernels.cu")])
+        return cls.module
+
+
+cppmodule = CppModule.instance()
+
+
+def testkernel(mat_A: torch.Tensor) -> torch.Tensor:
+    out = cppmodule.testkernel(mat_A)
+
+    return out
+
+
+def copykernel(mat_A: torch.Tensor) -> torch.Tensor:
+    out = cppmodule.copykernel(mat_A)
+
+    return out
+
+
+def mmkernelv1(mat_A: torch.Tensor, mat_B: torch.Tensor) -> torch.Tensor:
+    out = cppmodule.mmkernelv1(mat_A, mat_B)
+
+    return out
