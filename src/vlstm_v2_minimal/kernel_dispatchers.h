1fddc7572209d4b50c4f77c66d3ac1a62b282350
Maximilian Beck <beck@ml.jku.at>
2023-11-13 17:06:53 +0100

    copy v1 to a v2 minimal version as reference

---
+++ src/vlstm_v2_minimal/kernel_dispatchers.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// #include <torch/extension.h>
+
+namespace vlstm {
+
+namespace kernel_dispatchers {
+template <typename scalar_t>
+void copykernel_dispatch(scalar_t *mat_A, scalar_t *mat_B, int rows, int cols);
+
+template <typename scalar_t>
+void mmkernelv1_dispatch(scalar_t *matC, scalar_t *matA, scalar_t *matB, int m,
+                         int n, int k);
+
+template <typename scalar_t>
+void mmkernelv2_dispatch(scalar_t *matC, scalar_t *matA, scalar_t *matB, int m,
+                         int n, int k);
+
+} // namespace kernel_dispatchers
+
+} // namespace vlstm
