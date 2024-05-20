f61bccfe7f92d23d7cf1499dbec2a7151d456239
Maximilian Beck <beck@ml.jku.at>
2024-05-20 09:22:20 +0200

    complete tiled vlstm forward.

--- vlstm_fw_bw_torch/vlstm_fw_tiled.py
+++ vlstm_fw_bw_torch/vlstm_fw_tiled.py
@@ -90,7 +90,8 @@ def vlstm_fw_tiled_torch(
     q_tiles = torch.split(queries, bq_tile_size, dim=2)
     k_tiles = torch.split(keys, bkv_tile_size, dim=2)
     v_tiles = torch.split(values, bkv_tile_size, dim=2)
-
+    print(f"q_tiles: {len(q_tiles)}, {q_tiles[0].shape}")
+    print(f"kv_tiles: {len(k_tiles)}, {k_tiles[0].shape}")
     # TODO do not break causality!!!
     # TODO h scaling after c @ v multiplication missing
 
@@ -98,6 +99,7 @@ def vlstm_fw_tiled_torch(
     for q_idx, q_tile in enumerate(q_tiles):
         m_prev = torch.zeros((B, NH, bq_tile_size, 1), dtype=_dtype, device=_device)
         l_prev = torch.zeros((B, NH, bq_tile_size, 1), dtype=_dtype, device=_device)
+        n_prev = torch.zeros((B, NH, bq_tile_size, 1), dtype=_dtype, device=_device)
         h_tile = torch.zeros_like(q_tile)
         for kv_idx, (k_tile, v_tile) in enumerate(zip(k_tiles, v_tiles)):
             d_tile = log_D_matrix[
@@ -116,15 +118,21 @@ def vlstm_fw_tiled_torch(
                     s_tile * torch.exp(d_tile - m)
                 ).sum(dim=-1, keepdim=True)
 
-            c_tile = (s_tile * torch.exp(d_tile - m)) / (
-                torch.maximum(torch.abs(l), torch.exp(-m)) + eps
-            )
+            n = torch.maximum(torch.abs(l), torch.exp(-m))
+            c_tile = (s_tile * torch.exp(d_tile - m)) / (n + eps)
 
-            h_tile = (l_prev / l) * h_tile + c_tile @ v_tile
+            h_tile = torch.exp(m_prev - m) * (n_prev / n) * h_tile + c_tile @ v_tile
+            # h_tile = (l_prev / l) * h_tile + c_tile @ v_tile
+            # h_tile = (
+            #     torch.maximum(torch.abs(l_prev / l), torch.exp(-m_prev) / torch.exp(-m))
+            #     * h_tile
+            #     + c_tile @ v_tile
+            # )
             # h_tile = h_tile + c_tile @ v_tile
 
             m_prev = m
             l_prev = l
+            n_prev = n
         h_matrix[:, :, q_idx * bq_tile_size : (q_idx + 1) * bq_tile_size, :] = h_tile
 
     # # D matrix stabilization
@@ -145,4 +153,89 @@ def vlstm_fw_tiled_torch(
 
     # # retrieved values
     # retrieved_values = C_matrix_normalized @ values  # (B, NH, S, DH)
-    return h_matrix
+    return h_matrix, m, l
+
+
+# non-tiled reference implementation
+def vlstm_fw_torch_ref(
+    queries: torch.Tensor,
+    keys: torch.Tensor,
+    values: torch.Tensor,
+    igate_preact: torch.Tensor,
+    fgate_preact: torch.Tensor,
+    stabilize_rowwise: bool = True,
+    eps: float = 1e-6,
+) -> torch.Tensor:
+    """This is the core vLSTM operation in parallel form.
+    This version is stabilized. We control the range of exp() arguments by
+    ensuring that they are always smaller than 0.0 by subtracting the maximum.
+
+    Args:
+        queries (torch.Tensor): (B, NH, S, DH)
+        keys (torch.Tensor): (B, NH, S, DH)
+        values (torch.Tensor): (B, NH, S, DH)
+        igate_preact (torch.Tensor): (B, NH, S, 1)
+        fgate_preact (torch.Tensor): (B, NH, S, 1)
+        stabilize_rowwise (bool, optional): Wether to stabilize the combination matrix C rowwise (take maximum per row).
+            Alternative: Subtract the maximum over all rows. Defaults to True.
+
+    Returns:
+        torch.Tensor: (B, NH, S, DH), retrieved values
+    """
+
+    B, NH, S, DH = queries.shape
+    _dtype, _device = queries.dtype, queries.device
+
+    # forget gate matrix
+    log_fgates = F.logsigmoid(fgate_preact)  # (B, NH, S, 1)
+    ltr = torch.tril(
+        torch.ones(
+            (S, S),
+            dtype=torch.bool,
+            device=_device,
+        )
+    )
+    log_fgates_cumsum = torch.cat(
+        [
+            torch.zeros((B, NH, 1, 1), dtype=_dtype, device=_device),
+            torch.cumsum(log_fgates, dim=-2),
+        ],
+        dim=-2,
+    )  # (B, NH, S+1, 1)
+    # for each batch/head this is a matrix of shape (S+1, S+1) containing the cumsum of the log forget gate values
+    # in the second dimension (colum dimension). Each row has the same is a copy of the first row.
+    # First entry of each row is zero.
+    rep_log_fgates_cumsum = log_fgates_cumsum.repeat(
+        1, 1, 1, S + 1
+    )  # (B, NH, S+1, S+1)
+    # Now in each row cut off / subtract the forgetgate values of the later timesteps
+    # where col j > row i
+    _log_fg_matrix = rep_log_fgates_cumsum - rep_log_fgates_cumsum.transpose(
+        -2, -1
+    )  # (B, NH, S+1, S+1)
+    # Causal masking & selection of the correct submatrix, such that forgetgate at timestep t is not applied
+    # to the input at timestep t
+    log_fg_matrix = torch.where(
+        ltr, _log_fg_matrix[:, :, 1:, 1:], -float("inf")
+    )  # (B, NH, S, S)
+
+    # gate decay matrix D (combination of forget gate and input gate)
+    log_D_matrix = log_fg_matrix + igate_preact.transpose(-2, -1)  # (B, NH, S, S)
+    # D matrix stabilization
+    max_log_D, _ = torch.max(log_D_matrix, dim=-1, keepdim=True)  # (B, NH, S, 1)
+    log_D_matrix_stabilized = log_D_matrix - max_log_D  # (B, NH, S, S)
+    D_matrix = torch.exp(log_D_matrix_stabilized)  # (B, NH, S, S)
+
+    keys_scaled = keys / math.sqrt(DH)
+
+    # combination matrix C
+    qk_matrix = queries @ keys_scaled.transpose(-2, -1)  # (B, NH, S, S)
+    C_matrix = qk_matrix * D_matrix  # (B, NH, S, S)
+    l = C_matrix.sum(dim=-1, keepdim=True)
+    normalizer = torch.maximum(l.abs(), torch.exp(-max_log_D))  # (B, NH, S, 1)
+    # (B, NH, S, S)
+    C_matrix_normalized = C_matrix / (normalizer + eps)
+
+    # retrieved values
+    retrieved_values = C_matrix_normalized @ values  # (B, NH, S, DH)
+    return retrieved_values, max_log_D, l
