af5b354d2d44f4e25cc2db5de4f9bbb498c406d5
Maximilian Beck <beck@ml.jku.at>
2024-06-12 15:57:49 +0200

    add torch backward impl as a standalone function.

--- vlstm_formulations/vlstm_parallel_w_groupnorm.py
+++ vlstm_formulations/vlstm_parallel_w_groupnorm.py
@@ -13,7 +13,6 @@ def vlstm_parallel_fw_torch_w_groupnorm(
     values: torch.Tensor,
     igate_preact: torch.Tensor,
     fgate_preact: torch.Tensor,
-    stabilize_rowwise: bool = True,
     eps: float = 1e-6,
 ) -> torch.Tensor:
     """This is the core vLSTM operation in parallel form.
@@ -26,8 +25,6 @@ def vlstm_parallel_fw_torch_w_groupnorm(
         values (torch.Tensor): (B, NH, S, DH)
         igate_preact (torch.Tensor): (B, NH, S, 1)
         fgate_preact (torch.Tensor): (B, NH, S, 1)
-        stabilize_rowwise (bool, optional): Wether to stabilize the combination matrix C rowwise (take maximum per row).
-            Alternative: Subtract the maximum over all rows. Defaults to True.
 
     Returns:
         torch.Tensor: (B, NH, S, DH), retrieved values
@@ -72,13 +69,7 @@ def vlstm_parallel_fw_torch_w_groupnorm(
     # gate decay matrix D (combination of forget gate and input gate)
     log_D_matrix = log_fg_matrix + igate_preact.transpose(-2, -1)  # (B, NH, S, S)
     # D matrix stabilization
-    if stabilize_rowwise:
-        max_log_D, _ = torch.max(log_D_matrix, dim=-1, keepdim=True)  # (B, NH, S, 1)
-    else:
-        max_log_D = torch.max(log_D_matrix.view(B, NH, -1), dim=-1, keepdim=True)[
-            0
-        ].unsqueeze(-1)
-        # (B, NH, 1, 1)
+    max_log_D, _ = torch.max(log_D_matrix, dim=-1, keepdim=True)  # (B, NH, S, 1)
     log_D_matrix_stabilized = log_D_matrix - max_log_D  # (B, NH, S, S)
     D_matrix = torch.exp(log_D_matrix_stabilized)  # (B, NH, S, S)
 
@@ -93,12 +84,111 @@ def vlstm_parallel_fw_torch_w_groupnorm(
     # (B, NH, S, S)
     C_matrix_normalized = C_matrix / (normalizer + eps)
 
-    # retrieved values
-    retrieved_values = C_matrix_normalized @ values  # (B, NH, S, DH)
-    # out_scaled = (
-    #     retrieved_values - retrieved_values.mean(dim=-1, keepdim=True)
-    # ) / retrieved_values.std(dim=-1, keepdim=True, unbiased=False)
-    return retrieved_values
+    # hidden states
+    hiddens = C_matrix_normalized @ values  # (B, NH, S, DH)
+
+    return hiddens, normalizer, max_log_D
+
+
+def vlstm_parallel_bw_torch_w_groupnorm(
+    delta_Htilde: torch.Tensor,
+    queries: torch.Tensor,
+    keys: torch.Tensor,
+    values: torch.Tensor,
+    igate_preact: torch.Tensor,
+    fgate_preact: torch.Tensor,
+    var_n: torch.Tensor,
+    var_m: torch.Tensor,
+    eps: float = 1e-6,
+) -> tuple[torch.Tensor, ...]:
+    B, NH, S, DH = queries.shape
+    _dtype, _device = queries.dtype, queries.device
+
+    # compute var_D
+    # forget gate matrix
+    log_fgates = F.logsigmoid(fgate_preact)  # (B, NH, S, 1)
+    ltr = torch.tril(
+        torch.ones(
+            (S, S),
+            dtype=torch.bool,
+            device=_device,
+        )
+    )
+    log_fgates_cumsum = torch.cat(
+        [
+            torch.zeros((B, NH, 1, 1), dtype=_dtype, device=_device),
+            torch.cumsum(log_fgates, dim=-2),
+        ],
+        dim=-2,
+    )  # (B, NH, S+1, 1)
+    # for each batch/head this is a matrix of shape (S+1, S+1) containing the cumsum of the log forget gate values
+    # in the second dimension (colum dimension). Each row has the same is a copy of the first row.
+    # First entry of each row is zero.
+    rep_log_fgates_cumsum = log_fgates_cumsum.repeat(
+        1, 1, 1, S + 1
+    )  # (B, NH, S+1, S+1)
+    # Now in each row cut off / subtract the forgetgate values of the later timesteps
+    # where col j > row i
+    _log_fg_matrix = rep_log_fgates_cumsum - rep_log_fgates_cumsum.transpose(
+        -2, -1
+    )  # (B, NH, S+1, S+1)
+    # Causal masking & selection of the correct submatrix, such that forgetgate at timestep t is not applied
+    # to the input at timestep t
+    log_fg_matrix = torch.where(
+        ltr, _log_fg_matrix[:, :, 1:, 1:], -float("inf")
+    )  # (B, NH, S, S)
+    ltr_ig = torch.where(ltr, 0.0, -float("inf"))
+    ig_matrix = igate_preact.transpose(-2, -1) + ltr_ig  # (B, NH, S, S)
+    var_Dtilde = log_fg_matrix + ig_matrix
+    var_D = torch.exp(var_Dtilde - var_m)
+
+    # intermediate delta-errors
+    delta_C = delta_Htilde @ values.transpose(-2, -1) / (var_n + eps)
+
+    var_QK = queries @ (keys / math.sqrt(DH)).transpose(-2, -1)
+
+    delta_D = delta_C * var_QK
+
+    delta_Dtilde = delta_D * var_D
+
+    # compute fgate and igate preact delta errors
+    # delta_f: forget gate preactivation delta errors
+    ltr_dm1 = torch.tril(
+        torch.ones(
+            (S, S),
+            dtype=torch.bool,
+            device=_device,
+        ),
+        diagonal=-1,  #! Also mask out the diagonal as it is constant 1 in the D matrix
+    )
+    masked_deltaDtilde = torch.where(
+        ltr_dm1,
+        delta_Dtilde,
+        torch.tensor(0.0, device=_device, dtype=_dtype),
+    )
+
+    delta_fbar = torch.zeros((B, NH, S, 1), device=_device, dtype=_dtype)
+    # # first forget gate index (k=0) does not get a gradient (since it is not used in the forward pass)
+    # TODO implement this in a more efficient way
+    for k in range(1, S):
+        for j in range(k):
+            delta_fbar[:, :, k, 0] += (
+                masked_deltaDtilde[:, :, k:, j].view(B, NH, -1).sum(dim=-1)
+            )
+
+    delta_f = delta_fbar * torch.sigmoid(-fgate_preact)
+
+    # delta_i: input gate preactivation delta errors
+    delta_i = torch.sum(delta_Dtilde, dim=-2).unsqueeze_(-1)
+
+    # output delta-errors / gradients
+
+    delta_Q = (delta_C * var_D) @ (keys / math.sqrt(DH))
+    delta_K = (delta_C * var_D).transpose(-2, -1) @ (queries / math.sqrt(DH))
+
+    var_C = var_QK * var_D
+    delta_V = var_C.transpose(-2, -1) @ (delta_Htilde / (var_n + eps))
+    return delta_Q, delta_K, delta_V, delta_i, delta_f
 
 
 def vlstm_parallel_fwbw_torch_w_groupnorm(
@@ -107,16 +197,15 @@ def vlstm_parallel_fwbw_torch_w_groupnorm(
     values: torch.Tensor,
     igate_preact: torch.Tensor,
     fgate_preact: torch.Tensor,
-    stabilize_rowwise: bool = True,
     eps: float = 1e-6,
-) -> torch.Tensor:
-    retrieved_values = vLSTMParallelFwBwFull.apply(
-        queries, keys, values, igate_preact, fgate_preact, stabilize_rowwise, eps
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    hiddens, var_n, var_m = vLSTMParallelFwBwWithGroupNorm.apply(
+        queries, keys, values, igate_preact, fgate_preact, eps
     )
-    return retrieved_values
+    return hiddens, var_n, var_m
 
 
-class vLSTMParallelFwBwFull(torch.autograd.Function):
+class vLSTMParallelFwBwWithGroupNorm(torch.autograd.Function):
 
     @staticmethod
     def forward(
@@ -126,7 +215,73 @@ class vLSTMParallelFwBwFull(torch.autograd.Function):
         values: torch.Tensor,
         igate_preact: torch.Tensor,
         fgate_preact: torch.Tensor,
-        stabilize_rowwise: bool = True,
+        eps: float = 1e-6,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        hiddens, var_n, var_m = vlstm_parallel_fw_torch_w_groupnorm(
+            queries=queries,
+            keys=keys,
+            values=values,
+            igate_preact=igate_preact,
+            fgate_preact=fgate_preact,
+            eps=eps,
+        )
+        ctx.save_for_backward(
+            queries, keys, values, igate_preact, fgate_preact, var_n, var_m
+        )
+        return hiddens, var_n, var_m
+
+    @staticmethod
+    def backward(
+        ctx,
+        delta_Htilde: torch.Tensor,
+        grad_var_n_unused: torch.Tensor,
+        grad_var_m_unused: torch.Tensor,
+    ) -> tuple[torch.Tensor, ...]:
+        (queries, keys, values, igate_preact, fgate_preact, var_n, var_m) = (
+            ctx.saved_tensors
+        )
+        delta_Q, delta_K, delta_V, delta_i, delta_f = (
+            vlstm_parallel_bw_torch_w_groupnorm(
+                delta_Htilde=delta_Htilde,
+                queries=queries,
+                keys=keys,
+                values=values,
+                igate_preact=igate_preact,
+                fgate_preact=fgate_preact,
+                var_n=var_n,
+                var_m=var_m,
+            )
+        )
+        return delta_Q, delta_K, delta_V, delta_i, delta_f, None
+
+
+def vlstm_parallel_fwbw_torch_w_groupnorm_full(
+    queries: torch.Tensor,
+    keys: torch.Tensor,
+    values: torch.Tensor,
+    igate_preact: torch.Tensor,
+    fgate_preact: torch.Tensor,
+    eps: float = 1e-6,
+) -> torch.Tensor:
+    retrieved_values = vLSTMParallelFwBwWithGroupNormFull.apply(
+        queries, keys, values, igate_preact, fgate_preact, eps
+    )
+    return retrieved_values
+
+
+class vLSTMParallelFwBwWithGroupNormFull(torch.autograd.Function):
+    """A copy of vLSTMParallelFwBwWithGroupNorm where forward and backward are reimplemented
+    instead of fuction calls.
+    """
+
+    @staticmethod
+    def forward(
+        ctx,
+        queries: torch.Tensor,
+        keys: torch.Tensor,
+        values: torch.Tensor,
+        igate_preact: torch.Tensor,
+        fgate_preact: torch.Tensor,
         eps: float = 1e-6,
     ) -> torch.Tensor:
         """
@@ -136,8 +291,6 @@ class vLSTMParallelFwBwFull(torch.autograd.Function):
             values (torch.Tensor): (B, NH, S, DH)
             igate_preact (torch.Tensor): (B, NH, S, 1)
             fgate_preact (torch.Tensor): (B, NH, S, 1)
-            stabilize_rowwise (bool, optional): Wether to stabilize the combination matrix C rowwise (take maximum per row).
-                Alternative: Subtract the maximum over all rows. Defaults to True.
             eps (float, optional): Small constant to stabilize the division. Defaults to 1e-6.
         """
         B, NH, S, DH = queries.shape
@@ -182,12 +335,7 @@ class vLSTMParallelFwBwFull(torch.autograd.Function):
 
         var_Dtilde = log_fg_matrix + ig_matrix
 
-        if stabilize_rowwise:
-            var_M, _ = torch.max(var_Dtilde, dim=-1, keepdim=True)
-        else:
-            var_M = torch.max(var_Dtilde.view(B, NH, -1), dim=-1, keepdim=True)[
-                0
-            ].unsqueeze(-1)
+        var_M, _ = torch.max(var_Dtilde, dim=-1, keepdim=True)
 
         var_D = torch.exp(var_Dtilde - var_M)
 
