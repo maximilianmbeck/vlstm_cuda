fcf91d5f487b70cfdf11d9900b4f666bc93dceef
Maximilian Beck <beck@ml.jku.at>
2024-08-01 08:04:46 +0200

    add triton backward pass file. start from forward and adapt.

---
+++ src_triton/mlstm_parallel_fwbw_v0/mlstm_bw.py
@@ -0,0 +1,329 @@
+import math
+
+import torch
+import triton
+import triton.language as tl
+
+"""
+In this file:
+- parallel backward pass of the mlstm
+"""
+
+
+# We don't run auto-tuning every time to keep the tutorial fast. Keeping
+# the code below and commenting out the equivalent parameters is convenient for
+# re-tuning.
+configs = [
+    triton.Config({"BLOCK_Q": BQ, "BLOCK_KV": BKV}, num_stages=s, num_warps=w)
+    for BQ, BKV in [
+        (128, 128),
+        (128, 64),
+        (128, 32),
+        (128, 16),
+        (64, 64),
+        (64, 32),
+        (64, 16),
+        (32, 32),
+        (32, 16),
+        (16, 16),
+    ]
+    for s in [3, 4, 7]
+    for w in [4, 8]
+]
+
+
+def keep(conf):
+    BLOCK_M = conf.kwargs["BLOCK_Q"]
+    BLOCK_N = conf.kwargs["BLOCK_KV"]
+    if BLOCK_M * BLOCK_N < 128 * 128 and conf.num_warps == 8:
+        return False
+    return True
+
+
+BLOCK_Q = 16
+BLOCK_KV = 16
+
+
+# @triton.autotune(list(filter(keep, configs)), key=["N_CTX", "HEAD_DIM"])
+@triton.jit
+def _mlstm_bwd(
+    matQ,
+    matK,
+    matV,
+    vecI,
+    vecF_cs,
+    vecM,
+    vecN,
+    qk_scale,
+    matDeltaQ,
+    matDeltaK,
+    matDeltaV,
+    vecDeltaI,
+    stride_qz,
+    stride_qh,
+    stride_qm,
+    stride_qk,  #
+    stride_kz,
+    stride_kh,
+    stride_kn,
+    stride_kk,  #
+    stride_vz,
+    stride_vh,
+    stride_vk,
+    stride_vn,  #
+    stride_ifmn_z,
+    stride_ifmn_h,
+    stride_ifmn_m,
+    Z,
+    H,
+    N_CTX,  #
+    HEAD_DIM: tl.constexpr,  #
+    BLOCK_Q: tl.constexpr,  #
+    BLOCK_KV: tl.constexpr,  #
+    EPS: tl.constexpr = 1e-6,
+):
+    tl.static_assert(BLOCK_KV <= HEAD_DIM)
+    start_n_idx = tl.program_id(0)
+    off_hz = tl.program_id(1)
+    off_z = off_hz // H
+    off_h = off_hz % H
+    qkv_offset = off_z.to(tl.int64) * stride_qz + off_h.to(tl.int64) * stride_qh
+    ifmn_offset = (
+        off_z.to(tl.int64) * stride_ifmn_z + off_h.to(tl.int64) * stride_ifmn_h
+    )
+
+    # block pointers
+    Q_block_ptr = tl.make_block_ptr(
+        base=matQ + qkv_offset,
+        shape=(N_CTX, HEAD_DIM),
+        strides=(stride_qm, stride_qk),
+        offsets=(0, 0),
+        block_shape=(BLOCK_Q, HEAD_DIM),
+        order=(1, 0),
+    )
+    V_block_ptr = tl.make_block_ptr(
+        base=matV + qkv_offset,
+        shape=(N_CTX, HEAD_DIM),
+        strides=(stride_vk, stride_vn),
+        offsets=(start_n_idx * BLOCK_KV, 0),
+        block_shape=(BLOCK_KV, HEAD_DIM),
+        order=(1, 0),
+    )
+    K_block_ptr = tl.make_block_ptr(
+        base=matK + qkv_offset,
+        shape=(N_CTX, HEAD_DIM),
+        strides=(stride_kk, stride_kn),
+        offsets=(start_n_idx * BLOCK_KV, 0),
+        block_shape=(BLOCK_KV, HEAD_DIM),
+        order=(1, 0),
+    )
+    # TODO from here
+
+    # ? LOADING AND INITIALIZATION
+    # initialize accumulator
+    h_out = tl.zeros([BLOCK_Q, HEAD_DIM], dtype=tl.float32)
+
+    # load q: it will stay in SRAM throughout
+    q = tl.load(Q_block_ptr)
+
+    # define q_block_idxes for causal masking
+    q_offset = start_m_idx * BLOCK_Q
+    q_block_idxes = q_offset + tl.arange(0, BLOCK_Q)
+
+    # load vecF_cs: ifmn_offset defines the proper batch-head, q_offset defines the location
+    # in the sequence for the current thread block
+    vecF_cs_chunkQ_ptr = vecF_cs + ifmn_offset + q_offset + tl.arange(0, BLOCK_Q)
+    vecF_cs_chunkQ = tl.load(vecF_cs_chunkQ_ptr)
+    vecF_cs_chunkQ = vecF_cs_chunkQ.to(tl.float32)
+
+    # init l, m, n vectors
+    m_new = tl.zeros([BLOCK_Q], dtype=tl.float32) - float("inf")
+    m_old = tl.zeros([BLOCK_Q], dtype=tl.float32) - float("inf")
+    n_new = tl.zeros([BLOCK_Q], dtype=tl.float32)
+    n_old = tl.zeros([BLOCK_Q], dtype=tl.float32)
+    l_new = tl.zeros([BLOCK_Q], dtype=tl.float32)
+    l_old = tl.zeros([BLOCK_Q], dtype=tl.float32)
+
+    # ? MAIN LOOP
+    lo = 0
+    hi = (start_m_idx + 1) * BLOCK_Q
+    hi = tl.multiple_of(hi, BLOCK_Q)
+
+    K_block_ptr = tl.advance(K_block_ptr, (0, lo))
+    V_block_ptr = tl.advance(V_block_ptr, (lo, 0))
+
+    # loop over k, v and update accumulator
+    for start_n in range(lo, hi, BLOCK_KV):
+        start_n = tl.multiple_of(start_n, BLOCK_KV)
+        # -- compute matS --
+        k = tl.load(K_block_ptr)
+        matS = tl.dot(q, k)
+        matS = matS / qk_scale
+
+        # ? -- create gate matrix tile D --
+        # load vecF_cs_chunkKV
+        vecF_cs_vecI_chunkKV_offset = ifmn_offset + start_n + tl.arange(0, BLOCK_KV)
+        vecF_cs_chunkKV_ptr = vecF_cs + vecF_cs_vecI_chunkKV_offset
+        vecF_cs_chunkKV = tl.load(vecF_cs_chunkKV_ptr)
+        vecF_cs_chunkKV = vecF_cs_chunkKV.to(tl.float32)
+
+        # load vecI_chunkKV
+        vecI_ptr = vecI + vecF_cs_vecI_chunkKV_offset
+        vecI_chunkKV = tl.load(vecI_ptr)
+        vecI_chunkKV = vecI_chunkKV.to(tl.float32)
+
+        # compute D matrix
+        matD_log_fgates = vecF_cs_chunkQ[:, None] - vecF_cs_chunkKV[None, :]
+        matD = matD_log_fgates + vecI_chunkKV[None, :]
+
+        # ? -- causal masking --
+        #! TODO with this if I get a weird error: operation scheduled before its operands
+        if start_n >= q_offset:
+            # we are on diagonal
+            kv_block_idxes = start_n + tl.arange(0, BLOCK_KV)
+            mask = q_block_idxes[:, None] - kv_block_idxes[None, :]
+            matD = tl.where(mask >= 0, matD, -float("inf"))
+
+        # else: below diagonal
+
+        # ? -- compute m_state --
+        m_temp = tl.max(matD, axis=1)  # rowwise max
+        m_temp = tl.maximum(MINIMUM_MAX_VAL, m_temp)  # elementwise max
+        m_new = tl.maximum(m_old, m_temp)
+        m_ratio = tl.exp(m_old - m_new)
+
+        # ? -- compute matC --
+        matD_tilde = tl.exp(matD - m_new[:, None])
+        matC = matS * matD_tilde
+
+        # ? -- compute l_state --
+        # tl.fma did not bring performance improvement
+        l_temp = m_ratio * l_old
+        l_new = l_temp + tl.sum(matC, axis=1)
+
+        # ? -- compute n_state --
+        n_new = tl.maximum(tl.abs(l_new), tl.exp(-m_new))
+
+        # ? -- compute h_out -- update h_out --
+        # compute weighting factor
+        # tl.fdiv did not bring any performance improvement
+        h_out_old_weight = (m_ratio * n_old) / n_new
+        h_out = h_out * h_out_old_weight[:, None]
+
+        v = tl.load(V_block_ptr)
+
+        matC = matC / n_new[:, None]
+        matC = matC.to(tl.float16)
+        h_out = tl.dot(matC, v, h_out)
+
+        V_block_ptr = tl.advance(V_block_ptr, (BLOCK_KV, 0))
+        K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_KV))
+
+        l_old = l_new
+        m_old = m_new
+        n_old = n_new
+
+    # epilogue
+    tl.store(H_block_ptr, h_out.to(matH.type.element_ty))
+    vecM_ptr = vecM + ifmn_offset + q_offset + tl.arange(0, BLOCK_Q)
+    vecN_ptr = vecN + ifmn_offset + q_offset + tl.arange(0, BLOCK_Q)
+    tl.store(vecM_ptr, m_old.to(vecM.type.element_ty))
+    tl.store(vecN_ptr, n_old.to(vecN.type.element_ty))
+
+
+def mlstm_bw(
+    matDeltaHtilde: torch.Tensor,
+    matQ: torch.Tensor,
+    matK: torch.Tensor,
+    matV: torch.Tensor,
+    vecI: torch.Tensor,
+    vecF: torch.Tensor,
+    vecM: torch.Tensor,
+    vecN: torch.Tensor,
+    BLOCK_Q: int = BLOCK_Q,
+    BLOCK_KV: int = BLOCK_KV,
+) -> torch.Tensor:
+    # batch size, number of heads, sequence length, head dimension
+    BS, NH, SL, DH = matQ.shape
+    assert vecI.shape == (BS, NH, SL)
+    assert vecF.shape == (BS, NH, SL)
+    assert vecM.shape == (BS, NH, SL)
+    assert vecN.shape == (BS, NH, SL)
+    assert matDeltaHtilde.shape == (BS, NH, SL, DH)
+    assert matQ.shape == matK.shape == matV.shape
+
+    # shape constraints
+    HEAD_DIM_Q, HEAD_DIM_K = matQ.shape[-1], matK.shape[-1]
+    # when v is in float8_e5m2 it is transposed.
+    HEAD_DIM_V = matV.shape[-1]
+    assert HEAD_DIM_Q == HEAD_DIM_K and HEAD_DIM_K == HEAD_DIM_V
+    # assert HEAD_DIM_K in {16, 32, 64, 128, 256}
+
+    # grid = lambda args: (
+    #     triton.cdiv(matQ.shape[2], args["BLOCK_Q"]),
+    #     matQ.shape[0] * matQ.shape[1],
+    #     1,
+    # )
+    # fix grid for debugging
+    grid = lambda args: (
+        triton.cdiv(matQ.shape[2], BLOCK_Q),
+        matQ.shape[0] * matQ.shape[1],
+        1,
+    )
+    print(f"Triton grid: {grid(None)}, BLOCK_Q: {BLOCK_Q}, BLOCK_KV: {BLOCK_KV}")
+
+    ## ? preprocessing, initialization
+    matDeltaQ = torch.empty_like(matQ)
+    matDeltaK = torch.empty_like(matK)
+    matDeltaV = torch.empty_like(matV)
+
+    vecDeltaI = torch.zeros_like(vecI)
+
+    vecF_cs = torch.nn.functional.logsigmoid(vecF).cumsum(-1)
+    ## ? end preprocessing
+
+    # strides for matQ, matK, matV are same as matDeltaQ, matDeltaK, matDeltaV
+    _mlstm_bwd[grid](
+        matQ=matQ.contiguous(),
+        matK=matK.contiguous(),
+        matV=matV.contiguous(),
+        vecI=vecI.contiguous(),
+        vecF_cs=vecF_cs.contiguous(),
+        vecM=vecM.contiguous(),
+        vecN=vecN.contiguous(),
+        qk_scale=math.sqrt(HEAD_DIM_Q),
+        matDeltaQ=matDeltaQ,
+        matDeltaK=matDeltaK,
+        matDeltaV=matDeltaV,
+        vecDeltaI=vecDeltaI,
+        stride_qz=matQ.stride(0),
+        stride_qh=matQ.stride(1),
+        stride_qm=matQ.stride(2),
+        stride_qk=matQ.stride(3),
+        stride_kz=matK.stride(0),
+        stride_kh=matK.stride(1),
+        stride_kn=matK.stride(2),
+        stride_kk=matK.stride(3),
+        stride_vz=matV.stride(0),
+        stride_vh=matV.stride(1),
+        stride_vk=matV.stride(2),
+        stride_vn=matV.stride(3),
+        stride_ifmn_z=vecF_cs.stride(0),
+        stride_ifmn_h=vecF_cs.stride(1),
+        stride_ifmn_m=vecF_cs.stride(2),
+        Z=BS,
+        H=NH,
+        N_CTX=SL,
+        HEAD_DIM=HEAD_DIM_K,
+        BLOCK_Q=BLOCK_Q,
+        BLOCK_KV=BLOCK_KV,
+    )
+
+    ## ? postprocessing
+    # compute the vecDeltaFbar values with dfbar = rev_cumsum((q*dq - k*dk).sum(-1))
+    vecDeltaFbar_acc = (matQ * matDeltaQ - matK * matDeltaK).sum(-1)
+    vecDeltaFbar = vecDeltaFbar_acc.flip(-1).cumsum(-1).flip(-1)
+    vecDeltaF = vecDeltaFbar * torch.sigmoid(-vecF)
+    ## ? end postprocessing
+
+    return matDeltaQ, matDeltaK, matDeltaV, vecDeltaI, vecDeltaF
