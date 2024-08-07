ce1f631d9709f3831bfa0534b6fa935ea164b17e
Maximilian Beck <beck@ml.jku.at>
2024-08-07 10:37:13 +0200

    prepare benchmark for other new kernels & baselines.

--- src_triton/mlstm_parallel_fwbw_v1/benchmark_mlstm_parallel_fwbw.py
+++ src_triton/mlstm_parallel_fwbw_v1/benchmark_mlstm_parallel_fwbw.py
@@ -1,6 +1,8 @@
 import torch
 import triton
-from mlstm_parallel import mlstm_torch_autograd, mlstm_torch_ownbw, mlstm_triton
+from mlstm_parallel import mlstm_torch_autograd as mlstm_parallel_torch_autograd
+from mlstm_parallel import mlstm_torch_ownbw as mlstm_parallel_torch_ownbw
+from mlstm_parallel import mlstm_triton as mlstm_parallel_triton
 
 BATCH, N_HEADS = 1, 8
 
@@ -15,20 +17,20 @@ for HEAD_DIM in HEAD_DIMS:
             x_vals=[256, 512, 1024, 2048, 4096],  # [2**i for i in range(10, 15)],
             line_arg="provider",
             line_vals=[
-                "mlstm_pt_ag_compile_fwbw",
-                "mlstm_triton_fwbw",
-                "mlstm_pt_ag_compile_fw",
-                "mlstm_triton_fw",
+                "mlstm_parallel_pt_ag_compile_fwbw",
+                "mlstm_parallel_triton_fwbw",
+                "mlstm_parallel_pt_ag_compile_fw",
+                "mlstm_parallel_triton_fw",
             ],
             line_names=[
-                "mLSTM PT Autograd Compile FWBW",
-                "mLSTM Triton FWBW",
-                "mLSTM PT Autograd Compile FW",
-                "mLSTM Triton FW",
-            ],  # ["mLSTM PT", "mLSTM PT Compile", "mLSTM Triton"],
+                "mLSTM parallel PT Autograd Compile FWBW",
+                "mLSTM parallel Triton FWBW",
+                "mLSTM parallel PT Autograd Compile FW",
+                "mLSTM parallel Triton FW",
+            ],
             styles=[("red", "-"), ("blue", "-"), ("red", "--"), ("blue", "--")],
             ylabel="ms",
-            plot_name=f"mlstm_parallel_fwbw_fw-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}",
+            plot_name=f"mlstm_fwbw_fw-batch{BATCH}-head{N_HEADS}-d{HEAD_DIM}",
             args={
                 "H": N_HEADS,
                 "BATCH": BATCH,
@@ -43,6 +45,7 @@ def bench_flash_mlstm_fwbw(BATCH, H, N_CTX, HEAD_DIM, provider, device="cuda"):
     warmup = 25
     rep = 100
     dtype = torch.float16
+
     q = torch.randn(
         (BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True
     )
@@ -55,15 +58,19 @@ def bench_flash_mlstm_fwbw(BATCH, H, N_CTX, HEAD_DIM, provider, device="cuda"):
     ig = torch.randn((BATCH, H, N_CTX), dtype=dtype, device=device, requires_grad=True)
     fg = torch.randn((BATCH, H, N_CTX), dtype=dtype, device=device, requires_grad=True)
     if "triton" in provider:
-        fw_fn = lambda: mlstm_triton(q, k, v, ig, fg)
-
-    if "pt" in provider:
-        if "ag" in provider:
-            mlstm_pt = mlstm_torch_autograd
-        elif "obw" in provider:
-            mlstm_pt = mlstm_torch_ownbw
+        if "parallel" in provider:
+            fw_fn = lambda: mlstm_parallel_triton(q, k, v, ig, fg)
         else:
             raise ValueError(f"Unknown provider {provider}")
+
+    if "pt" in provider:
+        if "parallel" in provider:
+            if "ag" in provider:
+                mlstm_pt = mlstm_parallel_torch_autograd
+            elif "obw" in provider:
+                mlstm_pt = mlstm_parallel_torch_ownbw
+            else:
+                raise ValueError(f"Unknown provider {provider}")
 
         if "compile" in provider:
             mlstm_pt = torch.compile(mlstm_pt)
